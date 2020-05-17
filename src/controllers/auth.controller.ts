2f7ca78821dd6657637b11935c15c08343f36908
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-18 00:56:00 +0530

    completed token refreshing and req auth midware

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import axios from 'axios';
 import * as dotenv from 'dotenv';
 import * as uris from '../uris.conf';
@@ -6,7 +6,7 @@ import * as uris from '../uris.conf';
 dotenv.config();
 
 export async function handleAuthTokenRequest(req: Request, res: Response) {
-    
+
     const tokenRequestConfig = {
         client_id: process.env.CLIENT_ID,
         client_secret: process.env.CLIENT_SECRET,
@@ -15,10 +15,38 @@ export async function handleAuthTokenRequest(req: Request, res: Response) {
         code: decodeURIComponent(req.params['code'])
     }
     try {
-        const token = await axios.post(uris.oAuthTokenUri, tokenRequestConfig);   
+        const token = await axios.post(uris.oAuthTokenUri, tokenRequestConfig);
         res.json(token['data']);
     } catch (error) {
         console.log(error);
+    }
+}
+
+export async function handleRefreshAuthTokenRequest(req: Request, res: Response) {
+    const tokenRequestConfig = {
+        grant_type: "refresh_token",
+        refresh_token: req.body['refreshToken'],
+        client_id: process.env.CLIENT_ID,
+        client_secret: process.env.CLIENT_SECRET,
+    }
+
+    try {
+        const token = await axios.post(uris.oAuthTokenUri, tokenRequestConfig);
+        res.json(token['data']);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export async function verifyTokenMidware(req: Request, res: Response, next: NextFunction) {
+    const accessToken = req.headers['authorization'].split(' ')[1];
+    
+    try{
+        const verificationResponse = await axios.get(uris.oAuthTokenInfoUri + `?access_token=${accessToken}`);
+        return next();
+    } catch (err) {
+        console.log(err);
+        return res.json(err['data']);
     }
 
 }
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,3 +4,4 @@ import * as authControllers from '../controllers/auth.controller';
 export const router = express.Router();
 
 router.get('/authToken/:code', authControllers.handleAuthTokenRequest);
+router.post('/refreshAuthToken', authControllers.handleRefreshAuthTokenRequest);
