57c568515679f3333f6d8ae910425a5ba4bc7e3d
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-17 22:32:11 +0530

    installed cors

--- src/serve.ts
+++ src/serve.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import { PORT } from './serve.conf';
 import { router } from './routes/routes';
 
 const app = express();
 const HTTP_PORT: number = Number(process.env['PORT']) || PORT;
 
+app.use(cors());
 app.use('/', router);
 
 app.listen(HTTP_PORT, () => {
