4a504a0e75c71543efec1ddce2883ee2a7a22585
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-19 16:12:59 +0530

    added InternalServerError exception

--- src/customExceptions/generic/generic.errCodes.ts
+++ src/customExceptions/generic/generic.errCodes.ts
@@ -1,1 +1,2 @@
 export const notFound = 'RESOURCE_NOT_FOUND';
+export const serverError = 'INTERNAL_SERVER_ERROR';
--- src/customExceptions/generic/generic.exceptions.ts
+++ src/customExceptions/generic/generic.exceptions.ts
@@ -2,12 +2,17 @@ import * as codes from './generic.errCodes';
 import { CustomExceptionTemplate } from '../exception.model';
 
 export class ResourceNotFound extends CustomExceptionTemplate {
-    code: string;
-    responseCode: number;
-
     constructor(message: string, responseCode: number, payload?: object) {
         super(message, codes.notFound, responseCode, payload);
         this.name = 'ResourceNotFoundError';
+        this.stack = `${this.message}\n${new Error().stack}`;
+    }
+}
+
+export class InternalServerError extends CustomExceptionTemplate {
+    constructor(message: string, responseCode: number, payload?: object) {
+        super(message, codes.serverError, responseCode, payload);
+        this.name = 'InternalServerError';
         this.stack = `${this.message}\n${new Error().stack}`;
     }
 }
