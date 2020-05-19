7ca7c2aa1518c6379e5c44e88803602033eeb934
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-19 23:07:51 +0530

    mongoose connect and added db uri to uris.conf

--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose, { ConnectionOptions } from 'mongoose';
 import { PORT } from './serve.conf';
 import { authRouter } from './routes/auth.routes';
+import { buzzRouter } from './routes/buzz.routes';
+import { dbConnectionUri } from './uris.conf';
 
 const app = express();
 const HTTP_PORT: number = Number(process.env['PORT']) || PORT;
@@ -9,7 +12,24 @@ const HTTP_PORT: number = Number(process.env['PORT']) || PORT;
 app.use(express.json());
 app.use(cors());
 app.use('/auth', authRouter);
+app.use('/buzz', buzzRouter);
 
-app.listen(HTTP_PORT, () => {
-  console.log(`SERVER LISTENING ON PORT: ${HTTP_PORT}`);
+const connectOptions: ConnectionOptions = {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}
+mongoose.connect(dbConnectionUri, connectOptions);
+
+mongoose.connection.on('error', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+mongoose.connection.on('connected', (err, res) => {
+  console.log("mongoose is connected");
+  
+  app.listen(HTTP_PORT, () => {
+    console.log(`SERVER LISTENING ON PORT: ${HTTP_PORT}`);
+  });
 });
--- src/uris.conf.ts
+++ src/uris.conf.ts
@@ -1,3 +1,7 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const oAuthTokenUri = 'https://oauth2.googleapis.com/token';
 export const oAuthTokenInfoUri = 'https://oauth2.googleapis.com/tokeninfo';
 export const oAuthTokenRevokeUri = 'https://oauth2.googleapis.com/revoke';
+export const dbConnectionUri = `mongodb+srv://nilesh:${process.env.DB_SECRET}@cluster0-4f2ih.mongodb.net/TTN_Digital?retryWrites=true&w=majority`
