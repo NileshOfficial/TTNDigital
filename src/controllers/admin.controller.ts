118522eda0e2d2877986e2f7d1a52f0af1352147
NileshOfficial <nilesh.kumar@tothenew.com>
2020-05-26 22:35:06 +0530

    added isAdmin route

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,10 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
-import { addAdmin as saveAdmin } from '../services/admins.service';
+import * as adminService from '../services/admins.service';
 
 export async function addAdmin(req: Request, res: Response, next: NextFunction) {
     try {
-        const result = await saveAdmin(req.body);
+        const result = await adminService.addAdmin(req.body);
         res.json(result);
+    } catch (err) {
+        next(err);
+    }
+}
+
+export async function isAdmin(req: Request, res: Response, next: NextFunction) {
+    try {
+        const result = await adminService.isAdmin(req['userProfile']['email']);
+        res.json({ admin: result });
     } catch (err) {
         next(err);
     }
