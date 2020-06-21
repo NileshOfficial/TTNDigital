9fab2a13dbe4cdf84b41ce5149a5cff393033a70
livingscripts <nilesh22.a67@gmail.com>
2020-06-21 14:08:07 +0530

    added user router and department route now check for su access

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,16 +1,50 @@
-import { Router } from 'express';
-import { authRouter } from '../routes/auth.routes';
-import { buzzRouter } from '../routes/buzz.routes';
-import { complaintsRouter } from '../routes/complaints.routes';
-import { adminRouter } from '../routes/admin.routes';
-import * as genericMidwares from '../middlewares/generic.midwares';
-import * as authMidwares from '../middlewares/auth.midware';
+import { Router } from "express";
+import { authRouter } from "../routes/auth.routes";
+import { buzzRouter } from "../routes/buzz.routes";
+import { complaintsRouter } from "../routes/complaints.routes";
+import { adminRouter } from "../routes/admin.routes";
+import { userRouter } from "../routes/user.routes";
+import { departmentRouter } from "./department.routes";
+import * as genericMidwares from "../middlewares/generic.midwares";
+import * as authMidwares from "../middlewares/auth.midware";
 
 export const globalRouter = Router();
 
-globalRouter.use('/auth', authRouter);
-globalRouter.use('/buzz', authMidwares.retrieveAuthHeadersMidware, authMidwares.verifyTokenMidware, authMidwares.validateIdTokenMidware, buzzRouter);
-globalRouter.use('/complaints', authMidwares.retrieveAuthHeadersMidware, authMidwares.verifyTokenMidware, authMidwares.validateIdTokenMidware, complaintsRouter);
-globalRouter.use('/admin', authMidwares.retrieveAuthHeadersMidware, authMidwares.verifyTokenMidware, authMidwares.validateIdTokenMidware, adminRouter);
+globalRouter.use("/auth", authRouter);
+globalRouter.use(
+    "/buzz",
+    authMidwares.retrieveAuthHeadersMidware,
+    authMidwares.verifyTokenMidware,
+    authMidwares.validateIdTokenMidware,
+    buzzRouter
+);
+globalRouter.use(
+    "/complaints",
+    authMidwares.retrieveAuthHeadersMidware,
+    authMidwares.verifyTokenMidware,
+    authMidwares.validateIdTokenMidware,
+    complaintsRouter
+);
+globalRouter.use(
+    "/admin",
+    authMidwares.retrieveAuthHeadersMidware,
+    authMidwares.verifyTokenMidware,
+    authMidwares.validateIdTokenMidware,
+    adminRouter
+);
+globalRouter.use(
+    "/user",
+    authMidwares.retrieveAuthHeadersMidware,
+    authMidwares.verifyTokenMidware,
+    userRouter
+);
+globalRouter.use(
+    "/department",
+    authMidwares.retrieveAuthHeadersMidware,
+    authMidwares.verifyTokenMidware,
+    authMidwares.validateIdTokenMidware,
+    authMidwares.checkPrivileges("su"),
+    departmentRouter
+);
 globalRouter.use(genericMidwares.handleWildCardRequests);
 globalRouter.use(genericMidwares.errorHandlingMidware);
