056e118a50a7a587b2f2a971cbc6ba9fa96bfc66
livingscripts <nilesh22.a67@gmail.com>
2020-06-24 16:41:28 +0530

    added file upload to edit buzz

--- src/routes/buzz.routes.ts
+++ src/routes/buzz.routes.ts
@@ -5,11 +5,11 @@ export const buzzRouter = express.Router();
 
 buzzRouter.route('/')
     .get(buzzControllers.getBuzzes)
-    .post(buzzControllers.upload.array('files'), buzzControllers.createBuzz);
+    .post(buzzControllers.upload.array('files', 10), buzzControllers.createBuzz);
 
 buzzRouter.route('/:id')
     .delete(buzzControllers.deleteBuzz)
-    .patch(buzzControllers.updateBuzz);
+    .patch(buzzControllers.upload.array('files', 10), buzzControllers.updateBuzz);
 
 buzzRouter.patch('/like/:id', buzzControllers.updateLikes);
 buzzRouter.patch('/dislike/:id', buzzControllers.updateDisLikes);
